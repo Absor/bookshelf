33110eae4bc2e65b49a7e24f2468d34fbb467151
Heikki Haapala <hhaapala@gmail.com>
2014-03-30 01:09:09 +0200

    heroku config

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+module.exports = function(grunt) {
+  'use strict';
+
+  grunt.initConfig({
+    hub: {
+      all: {
+        src: ['ngapp/Gruntfile.js'],
+        tasks: ['build']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-hub');
+
+  grunt.registerTask('default', ['hub']);
+
+  grunt.registerTask('heroku:production', ['hub']);
+
+};
